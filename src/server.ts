9e7be690635fb59ffaa82d323c078df2964af8ba
Samuel Colvin <s@muelcolvin.com>
2021-06-26 18:18:19 +0100

    improve dev server, fix #2

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
 #!/usr/bin/env node
 
 import path from 'path'
-import express from 'express'
+import express, {Response as ExpressResponse} from 'express'
 import webpack from 'webpack'
+import livereload from 'livereload'
 import {makeEdgeEnv, EdgeKVNamespace, EdgeEnv} from './index'
 import live_fetch from './live_fetch'
+import {catArraysBufferViews, encode} from 'edge-mock/utils'
 
 export interface Config {
   webpack_config: string
   dist_path: string
   dist_assets_path: string
   prepare_key: (f: string) => string
+  livereload: boolean
   port: number
 }
+
+const livereload_port = 35729
 
 declare const global: any
 
@@ -24,8 +29,9 @@ async function load_config(): Promise<Config> {
   let config: Record<string, any> = {}
   try {
     config = await import(dev_server_config)
+    console.log('edge-mock-config.js found, using it for config')
   } catch (e) {
-    console.log('error importing:', e)
+    console.log('edge-mock-config.js not found, using default config')
   }
 
   config.webpack_config = config.webpack_config || path.join(cwd, 'webpack.config')
@@ -33,6 +39,9 @@ async function load_config(): Promise<Config> {
   config.dist_assets_path = config.dist_assets_path || path.join(cwd, 'dist/assets')
   config.prepare_key = config.prepare_key || default_prepare_key
   config.port = config.port || 3000
+  if (!('livereload' in config)) {
+    config.livereload = true
+  }
 
   return config as Config
 }
@@ -41,7 +50,23 @@ interface MultiStats {
   toString(options?: any): string
 }
 
-async function start_webpack(config: Config): Promise<EdgeEnv> {
+class WebpackState {
+  protected _error: Error | null = null
+
+  get error(): Error | null {
+    return this._error
+  }
+
+  clearError(): void {
+    this._error = null
+  }
+
+  setError(err: Error): void {
+    this._error = err
+  }
+}
+
+async function start_webpack(config: Config): Promise<[EdgeEnv, WebpackState]> {
   const kv_namespace = new EdgeKVNamespace()
 
   const global_extra = {
@@ -50,40 +75,69 @@ async function start_webpack(config: Config): Promise<EdgeEnv> {
     fetch: live_fetch,
   }
   const env = makeEdgeEnv(global_extra)
+  const webpack_state = new WebpackState()
 
   const wp_config = await import(config.webpack_config)
-  const watch_promise: Promise<MultiStats> = new Promise((resolve, reject) => {
+
+  async function on_webpack_success(stats: MultiStats): Promise<void> {
+    console.log(stats.toString('minimal'))
+    delete require.cache[require.resolve(config.dist_path)]
+
+    await kv_namespace._add_files(config.dist_assets_path, config.prepare_key)
+    global.__STATIC_CONTENT_MANIFEST = kv_namespace._manifestJson()
+    try {
+      await import(config.dist_path)
+      webpack_state.clearError()
+    } catch (err) {
+      // console.error(`Error importing ${path.relative('.', config.dist_path)}:\n`, e)
+      webpack_state.setError(err)
+    }
+  }
+
+  async function on_webpack_failure(err: Error): Promise<void> {
+    console.error(err)
+    webpack_state.setError(err)
+  }
+
+  const watch_promise: Promise<void> = new Promise(resolve => {
     webpack(wp_config.default).watch({}, (err, stats) => {
       if (err) {
-        reject(err)
+        on_webpack_failure(err).then(() => resolve())
       } else {
-        resolve(stats as MultiStats)
+        on_webpack_success(stats as MultiStats).then(() => resolve())
       }
     })
   })
 
-  const stats = await watch_promise
-  console.log(stats.toString('minimal'))
-  delete require.cache[require.resolve(config.dist_path)]
-
-  await kv_namespace._add_files(config.dist_assets_path, config.prepare_key)
-  global.__STATIC_CONTENT_MANIFEST = kv_namespace._manifestJson()
-  await import(config.dist_path)
-
-  return env
+  await watch_promise
+  return [env, webpack_state]
 }
 
-function run_server(config: Config, env: EdgeEnv) {
+function on_internal_error(res: ExpressResponse, message: string, error: Error, status = 504) {
+  console.error(message, error)
+  res.status(status)
+  res.set({'content-type': 'text/plain'})
+  res.send(`${message}:\n\n${error.message}\n${error.stack}\n`)
+}
+
+function run_server(config: Config, env: EdgeEnv, webpack_state: WebpackState) {
   const app = express()
+  if (config.livereload) {
+    const reload_server = livereload.createServer({delay: 300, port: livereload_port})
+    reload_server.watch(path.dirname(config.dist_path))
+  }
 
   app.all(/.*/, (req, res) => {
-    let listener: (event: FetchEvent) => void
+    if (webpack_state.error) {
+      on_internal_error(res, 'Failed to load worker code', webpack_state.error)
+      return
+    }
+
+    let listener: (event: FetchEvent) => any
     try {
       listener = env.getListener()
     } catch (err) {
-      console.error(err)
-      res.status(504)
-      res.send(err.toString())
+      on_internal_error(res, 'Error getting FetchEvent listener', err)
       return
     }
 
@@ -92,26 +146,42 @@ function run_server(config: Config, env: EdgeEnv) {
 
     const event = new FetchEvent('fetch', {request})
     event.respondWith = promise => {
-      Promise.resolve(promise).then(response => {
-        res.status(response.status)
-        res.set(Object.fromEntries(response.headers.entries()))
-        response.arrayBuffer().then(ab => {
-          res.send(new Buffer(ab))
+      Promise.resolve(promise)
+        .then(response => {
+          res.status(response.status)
+          res.set(Object.fromEntries(response.headers.entries()))
+          response.arrayBuffer().then(ab => {
+            if ((response.headers.get('content-type') || '').includes('text/html')) {
+              const body = [
+                new Uint8Array(ab),
+                encode(`\n\n<script src="http://localhost:${livereload_port}/livereload.js?snipver=1"></script>\n`),
+              ]
+              res.send(Buffer.from(catArraysBufferViews(body)))
+            } else {
+              res.send(Buffer.from(ab))
+            }
+          })
         })
-      })
+        .catch(err => on_internal_error(res, 'Internal Error awaiting response promise', err))
     }
-    listener(event)
+
+    try {
+      Promise.resolve(listener(event)).catch(err => on_internal_error(res, 'Internal Error running web-worker', err))
+    } catch (err) {
+      on_internal_error(res, 'Internal Error running web-worker', err)
+    }
   })
 
   app.listen(config.port, () => {
-    console.log(`dev app running at http://localhost:${config.port}`)
+    console.log(`dev app running at http://localhost:${config.port}, livereload: ${config.livereload}`)
   })
 }
 
 async function main(): Promise<void> {
   const config = await load_config()
-  const env = await start_webpack(config)
-  await run_server(config, env)
+  // console.log('starting webpack, config: %o', config)
+  const [env, wps] = await start_webpack(config)
+  await run_server(config, env, wps)
 }
 
 if (require.main === module) {
